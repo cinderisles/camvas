e3815ec49a0dcfc2559c16f66b8603241b207bdc
cinderisles <24627271+cinderisles@users.noreply.github.com>
2024-03-07 18:22:07 -0600

    add pandacss

--- packages/camvas-vue/src/App.vue
+++ packages/camvas-vue/src/App.vue
@@ -1,14 +1,64 @@
 <script setup lang="ts">
-import Worker from './worker?worker'
-
-const worker = new Worker()
+import { useStyles } from './composables/styles';
+const worker = new Worker(new URL('./worker.ts', import.meta.url), {
+  type: 'module',
+});
 
 worker.addEventListener('message', (e) => {
-  console.log('the thing', e.data)
-})
+  console.log('the thing', e.data);
+});
+
+const { css, wFull, hFull } = useStyles();
 </script>
 
 <template>
-  <div></div>
-</template>
+  <div
+    :class="
+      css(
+        {
+          display: 'flex',
+          flexDirection: 'column',
+          placeItems: 'center',
+        },
+        hFull,
+      )
+    "
+  >
+    <div
+      :class="
+        css(
+          {
+            paddingX: 10,
+            flexGrow: 1,
+          },
+          wFull,
+        )
+      "
+    >
+      <canvas :class="css(wFull)"></canvas>
+    </div>
 
+    <div
+      :class="
+        css(
+          {
+            bg: 'gray.800',
+            color: 'white',
+            padding: 2,
+          },
+          wFull,
+        )
+      "
+    >
+      <div
+        :class="
+          css({
+            display: 'flex',
+          })
+        "
+      ></div>
+
+      <button>select</button>
+    </div>
+  </div>
+</template>
---
+++ packages/camvas-vue/src/composables/styles.ts
@@ -0,0 +1,17 @@
+import { css } from '../../panda/css';
+
+export const useStyles = () => {
+    const wFull = css.raw({
+        w: '100%',
+    })
+
+    const hFull = css.raw({
+        h: '100%',
+    })
+
+    return {
+        css,
+        wFull,
+        hFull
+    }
+}
